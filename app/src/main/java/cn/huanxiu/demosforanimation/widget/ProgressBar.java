646be3a75531d66edf751ec8156b6857c2f9badc
273089055@qq.com <273089055@qq.com>
2018-12-27 18:13:24 +0800

    add

--- app/src/main/java/cn/huanxiu/demosforanimation/widget/ProgressBar.java
+++ app/src/main/java/cn/huanxiu/demosforanimation/widget/ProgressBar.java
@@ -1,9 +1,35 @@
 package cn.huanxiu.demosforanimation.widget;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
 /**
  * 作者：liujinlong
  * 时间：2018/12/27
  * 功能：
  */
-public class ProgressBar {
+public class ProgressBar extends View{
+
+    public ProgressBar(Context context, AttributeSet attrs){
+        super(context,attrs);
+        Log.d("ProgressBar","jkdjdjd--");
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measureWidth=MeasureSpec.getSize(widthMeasureSpec);
+        Log.d("ProgressBar","measureWidth--"+measureWidth);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+
+
 }
