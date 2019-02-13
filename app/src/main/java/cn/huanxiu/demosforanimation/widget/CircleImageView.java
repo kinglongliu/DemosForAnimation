15dfafbc01a9dbae516ac86866bc66792e76b66d
273089055@qq.com <273089055@qq.com>
2019-02-13 14:06:32 +0800

    perfect the CircleImageView

--- app/src/main/java/cn/huanxiu/demosforanimation/widget/CircleImageView.java
+++ app/src/main/java/cn/huanxiu/demosforanimation/widget/CircleImageView.java
@@ -3,16 +3,21 @@ package cn.huanxiu.demosforanimation.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import cn.huanxiu.demosforanimation.R;
+import cn.huanxiu.demosforanimation.utils.CommonUtil;
 
 /**
  * 作者：liujinlong
@@ -28,7 +33,10 @@ public class CircleImageView extends android.support.v7.widget.AppCompatImageVie
     private int mEdgeColor;
     private float edgeWidth;
     private float radius;
-    private Paint mPaint;
+    private Paint mPaint,mBorderPaint;
+    private Shader mShader;
+    private Bitmap mBitmap;
+
 
     public CircleImageView(Context context){
         super(context);
@@ -45,25 +53,40 @@ public class CircleImageView extends android.support.v7.widget.AppCompatImageVie
         mEdgeColor=attributes.getColor(R.styleable.CircleImageView_circle_edge_color,default_edge_color);
         edgeWidth=attributes.getDimension(R.styleable.CircleImageView_circle_edge_width,default_circle_edge);
         radius=attributes.getDimension(R.styleable.CircleImageView_circle_radius,default_circle_radius);
-        Log.d("CircleImageView","mEdgeColor=="+mEdgeColor+"edgeWidth=="+edgeWidth+"radius=="+radius);
         init();
     }
 
     private void init(){
+        mBorderPaint=new Paint();
+        mBorderPaint.setAntiAlias(true);
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+        mBorderPaint.setStrokeWidth(edgeWidth);
+        mBorderPaint.setColor(mEdgeColor);
+
         mPaint=new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(edgeWidth);
-        mPaint.setColor(mEdgeColor);
+        mPaint.setStyle(Paint.Style.FILL);
+        mBitmap= CommonUtil.drawableToBitmap(getDrawable());
+        if(mBitmap!=null){
+            mShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            mPaint.setShader(mShader);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width=Math.min(getMeasuredWidth(),getMeasuredHeight());
+        int heigth=width;
+        setMeasuredDimension(width,heigth);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawCircle(100,100,radius,mPaint);
-        Drawable drawable=getDrawable();
-        if(drawable!=null){
-            Log.d("CircleImageView","drawable is not null");
-        }
+        if(getDrawable()==null)
+            return;
+        float radius=getWidth()/2;
+        canvas.drawCircle(radius,radius,radius-(int)edgeWidth,mPaint);
+        canvas.drawCircle(radius,radius,radius-(int)edgeWidth,mBorderPaint);
     }
 }
